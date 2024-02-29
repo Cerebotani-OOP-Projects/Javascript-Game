969f7a98f1995a1a32f744ca3e7486f7447699e5
Michael Saccone <michael@info0-00.cerebotani.it>
2024-02-29 11:03:52 +0100

    add sprite class

--- script/game.js
+++ script/game.js
@@ -33,7 +33,9 @@ class Timer {
 
 class Animation {
 
-    constructor(sources, interval) {
+    active;
+
+    constructor(sources) {
         this.imageSrcs = sources;
         this.images = [];
         for(let src of this.imageSrcs) {
@@ -42,20 +44,92 @@ class Animation {
             this.images.push(img);
         }
         this.currentFrame = 0;
+        this.active = false;
         this.frames = this.images.length;
+    }
+
+    start(interval) {
         this.timer = new Timer(interval);
+        this.active = true;
     }
 
     update() {
-        this.timer.update();
-        if(this.timer.onFire()) {
-            this.currentFrame = (this.currentFrame + 1) % this.frames;
+        if(this.active) {
+            this.timer.update();
+            if(this.timer.onFire()) {
+                this.currentFrame = (this.currentFrame + 1) % this.frames;
+            }
         }
     }
 
-    draw(ctx) {
-        ctx.drawImage(this.images[this.currentFrame], 50, 50, 175, 175);
+    stop() {
+        this.active = false;
+        this.currentFrame = 0;
     }
+
+    getCurrentImage() {
+        return this.images[this.currentFrame];
+    }
+
+}
+
+class Vector2D {
+    x;
+    y;
+
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    set(x, y) {
+        this.x = x;
+        this.y = y;
+    }
+    
+    add(x, y) {
+        this.x += x;
+        this.y += y;
+    }
+}
+
+class Sprite {
+    position;
+    speed;
+    animation;
+    moving;
+    image;
+
+    constructor(image_src) {
+        this.animation = new Animation(image_src)
+        this.position = new Vector2D(50, 200);
+        this.speed = new Vector2D(0, 0);
+        this.moving = false;
+        this.image = new Image();
+        this.image.src = image_src[0];
+    }
+
+    setSpeed(x, y) {
+        this.speed.set(x, y);
+    }
+
+
+    update() {
+        // if(this.moving) {
+        //     let interval = (80 - Math.abs(this.speed.x * 1.5))
+        //     this.animation.start(interval);
+        // }
+        // else {
+        //     this.animation.stop();
+        // }
+        this.animation.update();
+        // this.image = this.animation.getCurrentImage();
+    }
+
+    draw(ctx) {
+        ctx.drawImage(this.image, this.position.x, ctx.canvas.clientHeight - this.position.y, 175, 175);
+    }
+
 }
 
 class Sound {
@@ -92,7 +166,7 @@ class Game {
         this.canvas.style.backgroundImage = "url('" + BACKGROUND_IMG_SRC + "')";
         this.canvas.style.backgroundSize = "contain";
 
-        this.player = new Animation(PLAYER_SRC, 25);
+        this.player = new Sprite(PLAYER_SRC);
         this.bgMusic = new Sound("assets/audio/background.mp3");
     }
 
